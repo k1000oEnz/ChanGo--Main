27d0bcb03cd0aa89acb8041f248fda759384a890
noeccff <noeliacabezas228@gmail.com>
2024-08-16 03:38:05 -0300

    Modifique algunas cosas de la página pago, agregue el JS

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+// Validar y enviar el formulario
+var form = document.getElementById('paymentForm');
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    if(validarFormulario()){
+        alert('Pago procesado con éxito. Gracias por su compra!');
+        form.reset();
+    } else {
+        alert('Por favor, revise los campos del formulario.');
+    }
+});
+
+// Función para validar el formulario
+function validarFormulario() {
+    var campos = ['titular', 'tarjeta', 'vencimiento', 'cvv'];
+    var esValido = true;
+
+    for(var i = 0; i < campos.length; i++){
+        var campo = document.getElementById(campos[i]);
+        if(campo.value.trim() == ''){
+            alert('Por favor, complete el campo ' + campos[i] + '.');
+            esValido = false;
+            break;
+        }
+    }
+
+    if(esValido){
+        var tarjeta = document.getElementById('tarjeta').value.replace(/\s/g, '');
+        if(tarjeta.length != 16){
+            alert('El número de tarjeta debe tener 16 dígitos.');
+            esValido = false;
+        }
+    }
+
+    return esValido;
+}
