b7618c8fd7ef5c7aba1b52de462f8625260eddfa
Emanuel Gonzazlez <emmanuelgonzalezmanu11@gmail.com>
2024-08-18 17:51:25 -0300

    se realiazo la ventana de preguntas frecuentes

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,3 @@
-const d = document;
-const buttonShow = d.getElementById("showButton");
-
-// Definición del componente
 const MyComponents = () => {
   return React.createElement("div", { className: "renderQuestions" }, [
     React.createElement(
@@ -10,25 +6,130 @@ const MyComponents = () => {
       React.createElement("img", {
         key: 18,
         className: "bannerQuestion",
-        src: "../image/questions.jpeg",
+        src: "../image/Dibujo FAQ.svg",
       })
     ),
     React.createElement(
       "div",
-      { key: 17, className: "questions" },
-      React.createElement("h2", { key: 1 }, "Preguntas Frecuentes."),
+      { key: 19, className: "questions" },
+      React.createElement(
+        "h2",
+        { key: 1, className: "titleQuestions" },
+        "Preguntas Frecuentes."
+      ),
+      // Primera pregunta
       React.createElement(
         "div",
-        { key: 3, Id: "componenteRenden" },
-        React.createElement("h5", { key: 3 }, "¿Cómo me registro en ChanGo?"),
-        React.createElement("button", { key: 112, id: "showButton" }, "+")
+        { key: 3, id: "componenteRenden1" },
+        React.createElement("h5", { key: 4 }, "¿Cómo me registro en ChanGo?"),
+        React.createElement("button", { key: 5, id: "showButton1" }, "+")
       ),
       React.createElement(
         "div",
-        { key: 113, className: "renderRespuestas" },
+        { key: 6, className: "renderRespuestas" },
         React.createElement(
           "p",
-          { key: 2, className: "showP" },
+          { key: 7, className: "showP1" },
+          "Lorem ipsum dolor sit amet, consectetur adipisci, aliqua. Ut eniexercitation ullamco la ex ea commodo consequat."
+        )
+      ),
+      // Segunda pregunta
+      React.createElement(
+        "div",
+        { key: 8, id: "componenteRenden2" },
+        React.createElement(
+          "h5",
+          { key: 9 },
+          "¿Cómo puedo recuperar mi contraseña?"
+        ),
+        React.createElement("button", { key: 10, id: "showButton2" }, "+")
+      ),
+      React.createElement(
+        "div",
+        { key: 11, className: "renderRespuestas" },
+        React.createElement(
+          "p",
+          { key: 12, className: "showP2" },
+          "Lorem ipsum dolor sit amet, consectetur adipisci, aliqua. Ut eniexercitation ullamco la ex ea commodo consequat."
+        )
+      ),
+      // Tercera pregunta
+      React.createElement(
+        "div",
+        { key: 13, id: "componenteRenden3" },
+        React.createElement(
+          "h5",
+          { key: 14 },
+          "¿Cómo se usa el servicio de atención al cliente?"
+        ),
+        React.createElement("button", { key: 15, id: "showButton3" }, "+")
+      ),
+      React.createElement(
+        "div",
+        { key: 16, className: "renderRespuestas" },
+        React.createElement(
+          "p",
+          { key: 17, className: "showP3" },
+          "Lorem ipsum dolor sit amet, consectetur adipisci, aliqua. Ut eniexercitation ullamco la ex ea commodo consequat."
+        )
+      ),
+      // Cuarta pregunta
+      React.createElement(
+        "div",
+        { key: 18, id: "componenteRenden4" },
+        React.createElement(
+          "h5",
+          { key: 19 },
+          "¿Qué métodos de pago se aceptan?"
+        ),
+        React.createElement("button", { key: 20, id: "showButton4" }, "+")
+      ),
+      React.createElement(
+        "div",
+        { key: 21, className: "renderRespuestas" },
+        React.createElement(
+          "p",
+          { key: 22, className: "showP4" },
+          "Lorem ipsum dolor sit amet, consectetur adipisci, aliqua. Ut eniexercitation ullamco la ex ea commodo consequat."
+        )
+      ),
+      // Quinta pregunta
+      React.createElement(
+        "div",
+        { key: 23, id: "componenteRenden5" },
+        React.createElement(
+          "h5",
+          { key: 24 },
+          "¿Cómo puedo cancelar mi suscripción?"
+        ),
+        React.createElement("button", { key: 25, id: "showButton5" }, "+")
+      ),
+      React.createElement(
+        "div",
+        { key: 26, className: "renderRespuestas" },
+        React.createElement(
+          "p",
+          { key: 27, className: "showP5" },
+          "Lorem ipsum dolor sit amet, consectetur adipisci, aliqua. Ut eniexercitation ullamco la ex ea commodo consequat."
+        )
+      ),
+      // Sexta pregunta
+      React.createElement(
+        "div",
+        { key: 28, id: "componenteRenden6" },
+        React.createElement(
+          "h5",
+          { key: 29 },
+          "¿Cómo puedo contactar con el soporte técnico?"
+        ),
+        React.createElement("button", { key: 30, id: "showButton6" }, "+")
+      ),
+      React.createElement(
+        "div",
+        { key: 31, className: "renderRespuestas" },
+        React.createElement(
+          "p",
+          { key: 32, className: "showP6" },
           "Lorem ipsum dolor sit amet, consectetur adipisci, aliqua. Ut eniexercitation ullamco la ex ea commodo consequat."
         )
       )
@@ -37,17 +138,18 @@ const MyComponents = () => {
 };
 
 // Función para manejar el clic y alternar el estilo de display
-function manejarClick() {
-  const showP = document.querySelector(".showP");
+function manejarClick(event) {
+  const buttonId = event.target.id;
+  const showPClass = `showP${buttonId.replace("showButton", "")}`;
+  const showP = document.querySelector(`.${showPClass}`);
   if (showP) {
-    // Alterna el display entre none y flex
     if (showP.style.display === "none" || showP.style.display === "") {
       showP.style.display = "flex";
     } else {
       showP.style.display = "none";
     }
   } else {
-    console.error('Elemento con clase "showP" no encontrado.');
+    console.error(`Elemento con clase "${showPClass}" no encontrado.`);
   }
 }
 
@@ -57,13 +159,23 @@ const handleClickQuestions = () => {
   if (container) {
     ReactDOM.render(React.createElement(MyComponents), container);
 
-    // Configura el evento del clic para el botón dentro del componente
-    const buttonShow = document.getElementById("showButton");
-    if (buttonShow) {
-      buttonShow.addEventListener("click", manejarClick);
-    } else {
-      console.error('Botón con id "showButton" no encontrado.');
-    }
+    // Configura el evento del clic para los botones dentro del componente
+    const buttons = [
+      "showButton1",
+      "showButton2",
+      "showButton3",
+      "showButton4",
+      "showButton5",
+      "showButton6",
+    ];
+    buttons.forEach((id) => {
+      const button = document.getElementById(id);
+      if (button) {
+        button.addEventListener("click", manejarClick);
+      } else {
+        console.error(`Botón con id "${id}" no encontrado.`);
+      }
+    });
   } else {
     console.error('Elemento con id "setting-card" no encontrado.');
   }
