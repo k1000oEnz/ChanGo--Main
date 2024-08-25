ea28a2d01271876369f663b930770975638d1af6
Brenda <bcoronel4416@gmail.com>
2024-08-25 15:26:32 -0300

    Estilo del registro completo arregle el problema con el script y ya se puede utilizar el ocultamiento de contraseña

--- js/login_script.js
+++ js/login_script.js
@@ -1,14 +1,21 @@
-// Funcion para mostrar y ocultar contraseña
-var eye = document.getElementById('Eye_closed');
-var password = document.getElementById('password');
-eye.addEventListener("click", function(){
-    if(password.type == "password"){
-        password.type = "text";
-        eye.src = "../components/img/Login/login_eye_open.svg";
-        eye.style.opacity=0.8;
-    }else{
-        password.type = "password";
-        eye.src = "../components/img/Login/login_eye_closed.svg";
-        eye.style.opacity=1;
-    }
-})
+// Función para mostrar y ocultar contraseña del formulario de login y del registro
+function togglePassword(inputId, eyeId) {
+    var passwordField = document.getElementById(inputId);
+    var eyeIcon = document.getElementById(eyeId);
+    
+    eyeIcon.addEventListener("click", function() {
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+            eyeIcon.src = "../components/img/Login/login_eye_open.svg";
+            eyeIcon.style.opacity = 0.8;
+        } else {
+            passwordField.type = "password";
+            eyeIcon.src = "../components/img/Login/login_eye_closed.svg";
+            eyeIcon.style.opacity = 1;
+        }
+    });
+}
+
+// Asigne la función a los íconos de ojo
+togglePassword('password', 'Eye_closed');
+togglePassword('password2', 'Eye_closed2');
